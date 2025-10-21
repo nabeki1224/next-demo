9a10d667f6ca34034511f4bd84036556b9ba84d4
nabeki1224 <watanabe.kyo19991224@gmail.com>
2025-10-21 15:48:42 +0900

    hooks for crud

--- src/app/types.ts
+++ src/app/types.ts
@@ -1,3 +1,13 @@
+import { Post } from '@/generated/prisma'
+
+export type PostModalType = {
+  isOpen: boolean
+  onOpen: () => void
+  onClose: () => void
+  post: Post
+  setPost: (payload: Post) => void
+}
+
 export type ModalType = {
   isOpen: boolean
   onOpen: () => void
